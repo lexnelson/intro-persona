e799fb46d7a4ae7176b5b0398f5a517488b9c3d2
Lex Nelson <lexinelson597@gmail.com>
2021-06-18 17:47:44 -0400

    Add basic HTML, Add JS listners

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
-const h2 = document.createElement('h2');
-h2.textContent = "This content added by JavaScript";
-document.querySelector('body').appendChild(h2);
+const firstButton = document.getElementById("firstButton");
+const secondButton = document.getElementById("secondButton");
+const bio = document.getElementById("bio");
+const bioTwo = document.getElementById("bioTwo");
+function  hideElement(element) {
+    if (element.style.display !== "none"){
+        element.style.display = "none";
+    } else {
+        element.style.display = "block";
+    }
+}
+function hideBio() {
+    hideElement(bio);
+}
+function hideBioTwo() {
+    hideElement(bioTwo);
+}
+firstButton.addEventListener('click', hideBio);
+secondButton.addEventListener('click', hideBioTwo);
